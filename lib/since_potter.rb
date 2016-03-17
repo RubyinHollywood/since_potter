a12b71244c388878b677c04e292b49f479cae9b8
Stephanie Wilkinson <what.happens@gmail.com>
2016-03-16 19:45:24 -0700

    modifies .gitignore and moves logic from bin to lib

--- lib/since_potter.rb
+++ lib/since_potter.rb
@@ -1,3 +1,13 @@
 require 'since_potter/version'
 class SincePotter
+	def initialize
+		start = Time.now
+
+		loop do
+		  surpassed = "  #{(Time.now - start).to_i}"
+		  print surpassed
+		  print "\b" * surpassed.to_s.size
+		  sleep 1
+		end
+	end
 end
