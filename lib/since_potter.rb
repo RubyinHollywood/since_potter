9a1e9909b47477494d431a63d181b9013aa58068
Stephanie Wilkinson <what.happens@gmail.com>
2016-03-30 20:59:41 -0700

    figure out how to add the antigoal to the initialize method

--- lib/since_potter.rb
+++ lib/since_potter.rb
@@ -1,10 +1,16 @@
 require 'since_potter/version'
+
 class SincePotter
-	def initialize
+	def initialize goal:, antigoal:
+    @goal = goal
+    @antigoal = antigoal
 	end
 
 	def start
 		start_timer = Time.now
+    puts @antigoal
+    puts @goal
+    #For some reason, only the antigoal is printing.
 
 		loop do
 		  surpassed = "  #{(Time.now - start_timer).to_i}"
