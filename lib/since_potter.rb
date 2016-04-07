64c4febaad49fd84a41efe5b074b2f354cea4a8d
Stephanie Wilkinson <what.happens@gmail.com>
2016-04-06 19:37:06 -0700

    add a thread to wait for keypress and extract printing goals

--- lib/since_potter.rb
+++ lib/since_potter.rb
@@ -1,22 +1,36 @@
 require 'since_potter/version'
+require 'io/console'
 
 class SincePotter
-	def initialize goal:, antigoal:
+	def initialize goal: nil, antigoal: nil
     @goal = goal
     @antigoal = antigoal
 	end
 
 	def start
+		print_goals
 		start_timer = Time.now
-    puts @antigoal
-    puts @goal
     #For some reason, only the antigoal is printing.
 
-		loop do
+		key_pressed = false
+
+		Thread.new do
+			STDIN.getch
+			key_pressed = true
+		end
+
+		until key_pressed do
 		  surpassed = "  #{(Time.now - start_timer).to_i}"
 		  print surpassed
 		  print "\b" * surpassed.to_s.size
 		  sleep 1
 		end
+
+		print_goals
+	end
+
+	def print_goals
+		puts @antigoal if @antigoal
+		puts @goal if @goal
 	end
 end
