41593c84136e1cf1780cd547353533a626cae186
Stephanie Wilkinson <what.happens@gmail.com>
2016-04-13 21:02:10 -0700

    add huamnize time

--- lib/since_potter.rb
+++ lib/since_potter.rb
@@ -19,28 +19,41 @@ class SincePotter
 		end
 
 		until key_pressed do
-		  @seconds_elapsed = "#{(Time.now - start_timer).to_i}"
-      print "\t"
-			print @seconds_elapsed
-		  print "\b" * @seconds_elapsed.to_s.size.next
+		  seconds_elapsed = (Time.now - start_timer).floor
+
+		  print humanize_time(seconds_elapsed)
+
+		  print "\b" * humanize_time(seconds_elapsed).to_s.size
 		  sleep 1
 		end
 
     outro
 	end
 
+	def humanize_time seconds_elapsed
+		if seconds_elapsed < 60
+	  	"\t#{seconds_elapsed} seconds"
+	  elsif seconds_elapsed < 3600
+	  	min, sec = seconds_elapsed.divmod 60
+	  	"\t#{min} minutes and #{sec} seconds"
+	  else 
+	  	hour, sec = seconds_elapsed.divmod 3600
+			min, sec = sec.divmod 60
+			"\t#{hour} hours #{min} minutes and #{sec} seconds"
+	  end
+	end
+
   def intro
     puts "Are you ready?? Press a key when you reach your goal."
     puts "Your goal is: #{@goal}"
-		puts "See how long you can last until #{@goal} is met."
 	end
 
 	def outro
 		puts "#{@goal} happened!"
-		puts "You lasted #{@seconds_elapsed} seconds."
   end
 
 	def print_goals
+		puts @antigoal if @antigoal
 		puts @goal if @goal
 	end
 end
